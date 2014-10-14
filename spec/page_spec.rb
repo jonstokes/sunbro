8cf2981c4dca4ce6192c0090b2ee27232c9cf5b2
Jon Stokes <jon@paperhatpress.com>
2014-10-14 09:22:08 -0500

    Initial spec passes

--- spec/page_spec.rb
+++ spec/page_spec.rb
@@ -11,21 +11,21 @@ describe Sunbro::Page do
     it "it scrubs invalid UTF-8 from @body by converting to UTF-16, then back again" do
       # See http://stackoverflow.com/a/8873922/1169868
       pending "Example"
+      fail
     end
   end
 
   describe "#doc", no_es: true do
     it "uses the correct Nokogiri parser to parse html or xml, or lets Nokogiri guess" do
       pending "Example"
+      fail
     end
   end
 
   describe "#should_parse_as?", no_es: true do
     Mocktra("www.retailer.com") do
       get '/1.html' do
-        File.open("#{Rails.root}/spec/fixtures/web_pages/www--retailer--com/1.html") do |file|
-          file.read
-        end
+        "<html><head><title>Title</title></head><body><p>Body text</p></body></html>"
       end
     end
 
@@ -33,16 +33,16 @@ describe Sunbro::Page do
       url = "http://www.retailer.com/1.html"
 
       page = @http.fetch_page(url)
-      expect(page.send(:should_parse_as?, :xml)).to be_false
-      expect(page.send(:should_parse_as?, :html)).to be_true
+      expect(page.send(:should_parse_as?, :xml)).to eq(false)
+      expect(page.send(:should_parse_as?, :html)).to eq(true)
 
       page = @http.fetch_page(url, force_format: :html)
-      expect(page.send(:should_parse_as?, :xml)).to be_false
-      expect(page.send(:should_parse_as?, :html)).to be_true
+      expect(page.send(:should_parse_as?, :xml)).to eq(false)
+      expect(page.send(:should_parse_as?, :html)).to eq(true)
 
       page = @http.fetch_page(url, force_format: :xml)
-      expect(page.send(:should_parse_as?, :xml)).to be_true
-      expect(page.send(:should_parse_as?, :html)).to be_false
+      expect(page.send(:should_parse_as?, :xml)).to eq(true)
+      expect(page.send(:should_parse_as?, :html)).to eq(false)
     end
   end
 end
