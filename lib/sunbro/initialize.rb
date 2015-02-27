50d92d33648345c1c468174d9c31bf58548ac9a5
Jon Stokes <jon@jonstokes.com>
2015-02-26 18:02:28 -0600

    Log out proxy

--- lib/sunbro/initialize.rb
+++ lib/sunbro/initialize.rb
@@ -1,1 +1,4 @@
-RestClient.proxy = Sunbro::Settings.proxy_url if Sunbro::Settings.proxy_url
+if Sunbro::Settings.proxy_url
+  puts "## Setting RestClient proxy to #{Sunbro::Settings.proxy_url}"
+  RestClient.proxy = Sunbro::Settings.proxy_url
+end
